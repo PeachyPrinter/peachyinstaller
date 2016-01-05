af2dc03d11c22128319349947363cdaad22e210d
James Townley <software@peachyprinter.com>
2016-01-05 16:03:47 -0600

    Started to add concept of installed config

--- windows/src/installer_api.py
+++ windows/src/installer_api.py
@@ -44,16 +44,47 @@ class InstallerAPIStub(InstallerAPIBase):
     def initialize(self):
         return True
 
+class Application(object):
+    def __init__(self, web_config, installed_config = None):
+        if installed_config and installed_config['id'] != web_config['id']:
+            raise Exception("Unexpected error processing config")
+        self.id = web_config['id']
+        self.name = web_config['name']['en-us']
+        self.available_version = web_config['version']
+        self.download_location = web_config['location']
+        self.relitive_install_path = web_config['install_path']
+        self.executable_path = web_config['executable']
+        if installed_config:
+            self.full_installed_path = installed_config['installed_path']
+            self.current_version = installed_config['version']
+        else:
+            self.full_installed_path = None
+            self.current_version = None
+
+    def __eq__(self, other):
+        return (
+            self.id == other.id and
+            self.name == other.name and
+            self.available_version == other.available_version and
+            self.download_location == other.download_location and
+            self.relitive_install_path == other.relitive_install_path and
+            self.executable_path == other.executable_path and
+            self.full_installed_path == other.full_installed_path and
+            self.current_version == other.current_version
+            )
+
 
 class InstallerAPI(InstallerAPIBase):
     supported_configuration_versions = [0, ]
     def __init__(self, config_url="http://www.github.com/peachyprinter/peachyinstaller/config.json"):
         self._config_url = config_url
 
-    def _check_config(self, config):
+    def _check_web_config(self, config):
         if "version" in config:
             if config["version"] not in self.supported_configuration_versions:
                 return (False, 10304,  "Configuration version too new installer upgrade required")
+            else:
+                return (True, 0, "Success")
         else:
             return (False, 10303, "Config is not valid")
 
@@ -64,11 +95,15 @@ class InstallerAPI(InstallerAPIBase):
         try:
             data = result.read()
             config = json.loads(data)
-            result, error, message = self._check_config(config)
+            result, error, message = self._check_web_config(config)
             if result is True:
-                self._config = config
+                self._web_config = config
+                return (True, "0", "Success")
             else:
                 return (False, error, message)
         except Exception as ex:
             print ex
             return(False, 10302, 'Data File Corrupt or damaged')
+
+    def get_items(self):
+        return [Application(app) for app in self._web_config['applications']]
--- windows/test/test_installer_api.py
+++ windows/test/test_installer_api.py
@@ -1,16 +1,29 @@
 import unittest
 import os
 import sys
+import json
 from mock import patch, MagicMock
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..',))
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'src'))
 
-from installer_api import InstallerAPI
+from installer_api import InstallerAPI, Application
 
 
 @patch('installer_api.urllib2')
 class InstallerAPITest(unittest.TestCase):
+
+    def get_sample_web_config(self):
+        return {
+            "id": 1,
+            "name": {
+                "en-us": "aa",
+                },
+            "version": "0.1.2",
+            "location": "http://someurl",
+            "install_path": "bb",
+            "executable": "cc.exe",
+        }
 
     def make_mock_response(self, code=200, data=""):
         response = MagicMock()
@@ -53,6 +66,78 @@ class InstallerAPITest(unittest.TestCase):
         result = test_installer_api.initialize()
 
         self.assertEqual(expected_result, result)
+
+    def test_get_items_should_return_a_list_of_available_items(self, mock_urllib2):
+        test_installer_api = InstallerAPI()
+        sample = '{"version": 0, "applications":[%s]}' % json.dumps(self.get_sample_web_config())
+        expected_result = [Application(self.get_sample_web_config())]
+        mock_urllib2.urlopen.return_value = self.make_mock_response(code=200, data=sample)
+        init_result = list(test_installer_api.initialize())
+        self.assertTrue(init_result[0], init_result[2])
+
+        result = test_installer_api.get_items()
+
+        self.assertEqual(expected_result, result)
+
+
+class ApplicationTest(unittest.TestCase):
+
+    def get_sample_web_config(self):
+        return {
+            "id": 1,
+            "name": {
+                "en-us": "aa",
+                },
+            "version": "0.1.2",
+            "location": "http://someurl",
+            "install_path": "bb",
+            "executable": "cc.exe",
+        }
+
+    def get_sample_installed_config(self):
+        return {
+            "id": 1,
+            "name": {
+                "en-us": "aa",
+                },
+            "version": "0.1.1",
+            "installed_path": "bb",
+    }
+
+    def test_raises_exception_if_ids_do_not_match(self):
+        file_config = self.get_sample_installed_config()
+        file_config['id'] = 234
+        web_config = self.get_sample_installed_config()
+        with self.assertRaises(Exception):
+            Application(web_config, file_config)
+
+    def test_uses_web_config_name_if_name_do_not_match(self):
+        file_config = self.get_sample_installed_config()
+        file_config['name'] = 'Bad Wolf'
+        web_config = self.get_sample_web_config()
+        app = Application(web_config, file_config)
+        self.assertEquals(web_config['name']['en-us'], app.name)
+
+    def test_has_correct_fields(self):
+        installed_config = self.get_sample_installed_config()
+        web_config = self.get_sample_web_config()
+        app = Application(web_config, installed_config)
+
+        self.assertEquals(web_config['id'], app.id)
+        self.assertEquals(web_config['name']['en-us'], app.name)
+        self.assertEquals(installed_config['version'], app.current_version)
+        self.assertEquals(web_config['version'], app.available_version)
+        self.assertEquals(web_config['location'], app.download_location)
+        self.assertEquals(web_config['install_path'], app.relitive_install_path)
+        self.assertEquals(installed_config['installed_path'], app.full_installed_path)
+        self.assertEquals(web_config['executable'], app.executable_path)
+
+    def test_two_identical_applications_are_equal(self):
+        installed_config = self.get_sample_installed_config()
+        web_config = self.get_sample_web_config()
+        app1 = Application(web_config, installed_config)
+        app2 = Application(web_config, installed_config)
+        self.assertEquals(app1, app2)
 
 if __name__ == '__main__':
     unittest.main()
