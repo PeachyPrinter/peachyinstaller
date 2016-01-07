17ad6ef7193102987b2a975759f227844bd135a8
James Townley <software@peachyprinter.com>
2016-01-07 09:54:24 -0600

    Added logging and tests around upzipping

--- windows/src/application_install.py
+++ windows/src/application_install.py
@@ -1,11 +1,16 @@
 import threading
 import urllib2
 import os
+import zipfile
+
+import logging
+logger = logging.getLogger('peachy')
 
 
 class InstallerException(Exception):
     def __init__(self, error_code, message):
         super(InstallerException, self).__init__(message)
+        logger.error("{} - {}".format(error_code, message))
         self.error_code = error_code
 
 
@@ -20,18 +25,22 @@ class InstallApplication(threading.Thread):
         self._report_status("Initializing")
 
     def _report_status(self, message):
+        logger.info(message)
         if self._status_callback:
             self._status_callback(message)
 
     def _report_complete(self, success, message):
+        logger.info(message)
         if self._complete_callback:
             self._complete_callback(success, message)
 
     def _fetch_zip(self, url):
+        logger.info("Downloading from: {}".format(url))
         response = urllib2.urlopen(url)
         if response.getcode() != 200:
             raise InstallerException(10501, "Got error {} accessing {}".format(response.getcode(), url))
         file_path = os.path.join(self._temp_file_location, url.split('/')[-1])
+        logger.info("Saving to: {}".format(file_path))
         try:
             with open(file_path, 'wb') as zip_file:
                 total_read = 0
@@ -40,20 +49,36 @@ class InstallApplication(threading.Thread):
                     if not chunk:
                         break
                     total_read += len(chunk)
+                    logger.debug("Reading chunk: {}".format(total_read))
                     zip_file.write(chunk)
             return file_path
         except IOError:
             raise InstallerException(10502, "Error creating file: {}".format(file_path))
+
+    def _unzip_files(self, file_path, destination):
+        logger.info("Unzipping {} into {}".format(file_path, destination))
+        try:
+            with zipfile.ZipFile(file_path, "r") as zip_file_handle:
+                zip_file_handle.extractall(destination)
+                return destination
+        except Exception as ex:
+            logger.error(ex.message)
+            raise InstallerException(10502, "Error unzipping file")
 
     def run(self):
         try:
             self._report_status("Downloading")
             file_path = self._fetch_zip(self._application.download_location)
             self._report_status("Unpacking")
+            temp_destination = os.path.join(self._temp_file_location, self._application.name)
+            temp_destination = self._unzip_files(file_path, temp_destination)
             self._report_status("Installing")
             self._report_status("Creating Shortcuts")
             self._report_status("Finalizing")
             self._report_complete(True, "Success")
         except InstallerException as ex:
             self._report_complete(False, ex.message)
+        except Exception as ex:
+            logger.error(ex)
+            raise
 
--- windows/test/test_application_installer.py
+++ windows/test/test_application_installer.py
@@ -1,4 +1,5 @@
 import unittest
+import logging
 import os
 import sys
 import time
@@ -11,7 +12,7 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'src'))
 from application_install import InstallApplication
 from mock import patch, MagicMock, mock_open
 
-
+@patch('application_install.zipfile.ZipFile')
 @patch('application_install.urllib2')
 class InstallApplicationTest(unittest.TestCase, TestHelpers):
     sleep_time = 0.1
@@ -23,14 +24,14 @@ class InstallApplicationTest(unittest.TestCase, TestHelpers):
         response.read = dataString.read
         return response
 
-    def test_run_kills_thread_correctly_on_exception(self, mock_urlib2):
+    def test_run_kills_thread_correctly_on_exception(self, mock_urlib2, mock_ZipFile):
         mock_urlib2.urlopen.side_effect = Exception("Failed")
         installer = InstallApplication(self.get_application(), '')
         installer.start()
         time.sleep(self.sleep_time)
         installer.join()
 
-    def test_run_downloads_zip_file_from_correct_path(self, mock_urlib2):
+    def test_run_downloads_zip_file_from_correct_path(self, mock_urlib2, mock_ZipFile):
         mock_urlib2.urlopen.return_value = "This is a zip file"
         app = self.get_application()
         installer = InstallApplication(app, '')
@@ -39,7 +40,7 @@ class InstallApplicationTest(unittest.TestCase, TestHelpers):
         installer.join()
         mock_urlib2.urlopen.assert_called_with(app.download_location)
 
-    def test_run_should_report_failure_if_response_is_not_200(self, mock_urlib2):
+    def test_run_should_report_failure_if_response_is_not_200(self, mock_urlib2, mock_ZipFile):
         mock_urlib2.urlopen.return_value = self.get_mock_response(code=504)
         app = self.get_application()
         mock_complete_callback = MagicMock()
@@ -49,7 +50,7 @@ class InstallApplicationTest(unittest.TestCase, TestHelpers):
         installer.join()
         mock_complete_callback.assert_called_with(False, "Got error 504 accessing {}".format(app.download_location))
 
-    def test_run_should_report_failure_if_cannot_open_file(self, mock_urlib2):
+    def test_run_should_report_failure_if_cannot_open_file(self, mock_urlib2, mock_ZipFile):
         mock_urlib2.urlopen.return_value = self.get_mock_response(code=200)
         app = self.get_application()
         mock_complete_callback = MagicMock()
@@ -63,7 +64,7 @@ class InstallApplicationTest(unittest.TestCase, TestHelpers):
         expected_path = os.path.join(os.getenv("TEMP"), app.download_location.split('/')[-1])
         mock_complete_callback.assert_called_with(False, "Error creating file: {}".format(expected_path))
 
-    def test_run_should_save_file(self, mock_urlib2):
+    def test_run_should_save_file(self, mock_urlib2, mock_ZipFile):
         expected_data = "abba" * 1024 * 1024
         mock_urlib2.urlopen.return_value = self.get_mock_response(code=200, data=expected_data)
         app = self.get_application()
@@ -79,24 +80,22 @@ class InstallApplicationTest(unittest.TestCase, TestHelpers):
             actual = ''.join([data[0][0] for data in mock_file.write.call_args_list])
             self.assertEquals(expected_data, actual)
 
-    def test_run_should_repoort_saving_file(self, mock_urlib2):
+    def test_run_should_repoort_saving_file(self, mock_urlib2, mock_ZipFile):
         expected_data = "abba" * 1024 * 1024
         mock_urlib2.urlopen.return_value = self.get_mock_response(code=200, data=expected_data)
         app = self.get_application()
-        mock_complete_callback = MagicMock()
         mock_open_file = mock_open()
         with patch('application_install.open', mock_open_file, create=True):
             mock_file = mock_open_file.return_value
             mock_open_file.return_value = mock_file
-            installer = InstallApplication(app, '', complete_callback=mock_complete_callback)
+            installer = InstallApplication(app, '')
             installer.start()
             time.sleep(self.sleep_time)
             installer.join()
             actual = ''.join([data[0][0] for data in mock_file.write.call_args_list])
             self.assertEquals(expected_data, actual)
 
-
-    def test_run_should_report_steps(self, mock_urlib2):
+    def test_run_should_report_steps(self, mock_urlib2, mock_ZipFile):
         mock_status_callback = MagicMock()
         expected_calls = ["Initializing", "Downloading", "Unpacking", "Installing", "Creating Shortcuts", "Finalizing"]
         mock_urlib2.urlopen.return_value = self.get_mock_response(code=200)
@@ -108,12 +107,44 @@ class InstallApplicationTest(unittest.TestCase, TestHelpers):
             installer = InstallApplication(app, '', status_callback=mock_status_callback)
             installer.start()
             time.sleep(self.sleep_time)
-            
             calls = [call[0][0] for call in mock_status_callback.call_args_list]
-            actual = ''.join([data[0][0] for data in mock_file.write.call_args_list])
             self.assertEquals(expected_calls, calls)
 
+    def test_run_should_unzip_dowwnloaded_file(self, mock_urlib2, mock_ZipFile):
+        mock_urlib2.urlopen.return_value = self.get_mock_response(code=200)
+        app = self.get_application()
+        expected_source_path = os.path.join(os.getenv("TEMP"), app.download_location.split('/')[-1])
+        expected_destination_path = os.path.join(os.getenv("TEMP"), app.name)
+        mock_zip_handle = mock_ZipFile.return_value.__enter__.return_value
+
+        mock_open_file = mock_open()
+        with patch('application_install.open', mock_open_file, create=True):
+            mock_file = mock_open_file.return_value
+            mock_open_file.return_value = mock_file
+            installer = InstallApplication(app, '')
+            installer.start()
+            time.sleep(self.sleep_time)
+
+            mock_ZipFile.assert_called_with(expected_source_path, 'r')
+            mock_zip_handle.extractall.assert_called_with(expected_destination_path)
+
+    def test_run_should_report_error_if_fails_to_unzip_dowwnloaded_file(self, mock_urlib2, mock_ZipFile):
+        mock_urlib2.urlopen.return_value = self.get_mock_response(code=200)
+        app = self.get_application()
+        mock_complete_callback = MagicMock()
+        mock_ZipFile.return_value.__enter__.side_effect = IOError("Something failed")
+
+        mock_open_file = mock_open()
+        with patch('application_install.open', mock_open_file, create=True):
+            mock_file = mock_open_file.return_value
+            mock_open_file.return_value = mock_file
+            installer = InstallApplication(app, '', complete_callback=mock_complete_callback)
+            installer.start()
+            time.sleep(self.sleep_time)
+
+            mock_complete_callback.assert_called_with(False, "Error unzipping file")
 
 if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level='INFO')
     unittest.main()
 
